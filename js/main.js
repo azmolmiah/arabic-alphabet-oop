05fdb3c1993b6cd51dfb0461a9b2b45ea47a263a
azmolmiah <azmol.miah.general@gmail.com>
2022-06-05 01:47:07 +0100

    completed ui with bootstrap and fontawsome, configured json file structure and tested with main js file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
+// Global variables
+let currentPageNum = 0;
+
+// Page constructor - every page will have a current page number, number of sections, and functions to navigate pages
+function Page(currentPageNumber, numOfSections) {
+  this.currentPageNumber = currentPageNumber;
+  this.numOfSections = numOfSections;
+  this.getNextPage = function () {
+    return (this.currentPageNumber = currentPageNum++);
+  };
+  this.getPrevPage = function () {
+    return (this.currentPageNumber = currentPageNum--);
+  };
+}
+
+// UI constructor
+function Letters() {
+  // Get letters
+  this.getLetters = async function (currentPageNumber) {
+    try {
+      const letters = await fetch("js/qaida.json");
+      const data = await letters.json();
+      return data.letters[currentPageNumber];
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
+
+// Event Listeners
+
 // On Windows or App load first page
 window.addEventListener("load", async () => {
   const letters = new Letters();
